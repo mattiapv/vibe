f7db77be5124989ea3ffec3068be5f62e6ffcb9f
Mattia Pavan <git@mattiapavan.dev>
2026-03-25 23:30:01 +0100

    auto-generate .gitignore in .vibe to prevent tracking disk image

--- src/main.rs
+++ src/main.rs
@@ -56,6 +56,7 @@ const DEFAULT_IMAGE_NAME: &str = "default";
 const INSTANCE_DIR_NAME: &str = ".vibe";
 const INSTANCE_DISK_IMAGE_NAME: &str = "instance.raw";
 include!(concat!(env!("OUT_DIR"), "/provisioning.rs"));
+const VIBE_GITIGNORE: &str = "# created by vibe automatically\n*\n";
 
 #[derive(Clone)]
 enum LoginAction {
@@ -1451,7 +1452,9 @@ fn ensure_instance_disk(
     }
 
     println!("Creating instance disk from {}...", template_raw.display());
-    std::fs::create_dir_all(instance_raw.parent().unwrap())?;
+    let instance_dir = instance_raw.parent().unwrap();
+    std::fs::create_dir_all(instance_dir)?;
+    fs::write(instance_dir.join(".gitignore"), VIBE_GITIGNORE)?;
     fs::copy(template_raw, instance_raw)?;
     Ok(())
 }
