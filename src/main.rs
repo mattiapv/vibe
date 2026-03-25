7e45b708728cbddaa0080096ea5cb3f4ec6ddd4a
Mattia Pavan <git@mattiapavan.dev>
2026-03-25 23:59:27 +0100

    allow .git in the vm

--- src/main.rs
+++ src/main.rs
@@ -358,7 +358,7 @@ Commands
                 // Discourage read/write of project dir subfolders within the VM.
                 // Note that this isn't secure, since the VM runs as root and could unmount this.
                 // I couldn't find an alternative way to do this --- the MacOS sandbox doesn't apply to the Apple Virtualization system =(
-                for subfolder in [".git", INSTANCE_DIR_NAME] {
+                for subfolder in [INSTANCE_DIR_NAME] {
                     if project_root.join(subfolder).exists() {
                         login_actions.push(Send(format!(r" mount -t tmpfs tmpfs {subfolder}")));
                     }
